be6de56d62b700966fc59f3e8e36e96f4883d2b6
angelo007 <angelohalabi2@gmail.com>
2017-03-13 17:52:04 -0400

    server v2
    test server v2

--- Server/Server.js
+++ Server/Server.js
@@ -3,4 +3,28 @@ if(filePath == '/')
 	filePath = '/index.html';
 
 filePath = __dirname+filePath;
-var extname = path.ext
+var extname = path.ext;
+var contentType = 'text/html';
+
+switch(extname) {
+	case '.js':
+		contentType = 'text/javascript';
+		break;
+	case '.css':
+		 contentType = 'text/css';
+		 break;
+}
+
+fs.exists(filePath,function(exists) {
+if(exists) {
+	fs.readFile(filepath,function(error,content) {
+		if(error) {
+			res.writeHEad(500);
+			res.end();
+		}
+		else {
+			res.writeHead(200, { 'Content-Type': contentType});
+			res.end(content, 'utf-8')
+		}
+	});
+}
